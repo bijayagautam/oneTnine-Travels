59fb3661c12e7491099c2f5a469ffacf71f2f508
Bijaya Gautam <bijayagautam8@gmail.com>
2020-01-25 16:47:44 -0500

    Routes and Port setup

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 const express = require("express");
+const exphbs  = require('express-handlebars');
 
 const app = express();
 
 //Allowing express to make static content avialable from the public
 app.use(express.static('public'))
 
+//Telling Express to set or register Handlebars as its' Template/View Engine
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+//Setting up routes
+app.get("/",(req,res)=>{
+    res.render("home",{
+        mainContent: "Welcome to oneTnine Travel."
+    })
+});
+
+app.get("/userRegistration",(req,res)=>{
+    res.render("userRegistration")
+});
+
+app.get("/roomListing",(req,res)=>{
+    res.render("roomListing")
+});
+
+//Creating Web Server
+const PORT=3000;
+app.listen(PORT,()=>{
+    console.log(`Web server is up and running!`)
+})
