9744e8394822586cc27dae4e8be2d97e2d09dcf3
Bijaya Gautam <n01399849@nj-00118-020.humber.org>
2020-01-29 21:45:56 -0500

    Added search box

--- public/js/index.js
+++ public/js/index.js
@@ -6,9 +6,18 @@ function showHideMenu() {
     document.getElementById("mainMenu").classList.toggle("show-hide");  
 }
 
+/*  Function: showHideSearchArea
+    Parameters: NA
+    Return: show-hide class
+    Description: Hide or show search area when clicked */
+function showHideSearchArea() {
+    document.getElementById("searchArea").classList.toggle("show-hide");  
+}
+
 // Load Event Listener
 window.addEventListener('load', () => {
     //Click Event Listener
     document.getElementById('hamMenu').addEventListener('click', showHideMenu);
+    document.getElementById('searchlens').addEventListener('click', showHideSearchArea);
 
 });
