d47933150f9faec1c46e4a31497e273a3d16d85b
Bijaya Gautam <bijayagautam8@gmail.com>
2020-04-07 20:04:33 -0400

    Updated roomDashboard and minor changes

--- public/js/index.js
+++ public/js/index.js
@@ -10,23 +10,9 @@ function showHideSearchArea() {
     document.getElementById("searchArea").classList.toggle("show-hide");  
 }
 
-function showHideC() {
-    document.getElementById("createRoomForm").classList.toggle("show-hide");  
-}
-function showHideU() {
-    document.getElementById("modifyRoomForm").classList.toggle("show-hide");  
-}
-function showHideD() {
-    document.getElementById("deleteRoomForm").classList.toggle("show-hide");  
-}
-
 // Load Event Listener
 window.addEventListener('load', () => {
     //Click Event Listener
     document.getElementById('hamMenu').addEventListener('click', showHideMenu);
     document.getElementById('searchlens').addEventListener('click', showHideSearchArea);
-    document.getElementById('createRoom').addEventListener('click', showHideC);
-    document.getElementById('modifyRoom').addEventListener('click', showHideU);
-    document.getElementById('deleteRoom').addEventListener('click', showHideD);
-
 });
